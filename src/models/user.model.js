e10ceca6cc5d39ee7eb1d4d1ba4d404c95797fad
sahnoun122 <khadijasahnoun70@gmail.com>
2025-10-08 15:11:35 +0100

    la creations de la fichier de jwt.js

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,27 +1,15 @@
 import mongoose from "mongoose";
-import { isLowercase } from "validator";
 
-const MemberSubShema = new mongoose.Schema({
-    user_id :{ type : mongoose.Schema.Types.ObjectId , ref : "User" },
-
-    status :{type : String , enum :["active" , "pending" , "left"] , default : "active"},
-    joinedAt : { type : Date , default : Date.now}
-} , {_id : false });
-
-const UserShema = new mongoose.Schema({
-    firstName : {type : String},
-    lastNam : {type : String},
-     email : {type : String , required : true , unique : true , Lowercase : true },
-     passwosrd : {type : String , required : true },
-     role : {type : String , enum :[ "particulier" , "admin"], default : "particulier"},
-     national_id : String,
-     kyc_status : {type : String , enum : ["pending" , "verified" , "rejected"] , default : "pending"},
-     photo_id_url : String,
-     selfie_url : String,
-     reliability_score : {type : Number , default : 0},
-     refreshTokens : [{token : String , createdAt : Date}],
-     createdAt : {type : Date , default : Date.now }
-
+const userSchema = new mongoose.Schema({
+  firstName: { type: String, required: true },
+  lastName:  { type: String, required: true },
+  email:     { type: String, required: true, unique: true },
+  password:  { type: String, required: true },
+  role: { type: String, enum: ["particulier", "admin"], default: "particulier" },
+  national_id: { type: String },
+  kyc_status: { type: String, enum: ["pending", "verified", "rejected"], default: "pending" },
+  reliability_score: { type: Number, default: 100 },
+  createdAt: { type: Date, default: Date.now }
 });
 
-export default mongoose.model("User" , UserShema);
+export default mongoose.model("User", userSchema);
