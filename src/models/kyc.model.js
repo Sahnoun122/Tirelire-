a7c01dd1ed8104834a2e6a2372ab5c604551e3ae
sahnoun122 <khadijasahnoun70@gmail.com>
2025-10-13 11:52:14 +0100

    la creations de la milddelware pour upload image

--- src/models/kyc.model.js
+++ src/models/kyc.model.js
@@ -1,20 +1,33 @@
 import mongoose from "mongoose";
 
-const kycSchema  = new mongoose.Schema({
-    userId : {
-        type : mongoose.Types.ObjectId,
-        ref : "User",
-        require : true,
-    },
-    fullName : String,
-    nationalId :String,
-    cardImage : String,
-    faceImage : String,
-    status : {
-        type : String,
-        enum : ["pending", "verified", "rejected"],
-        default : "pending",
-    }
-});
+const kycSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  fullName: {
+    type: String,
+    required: true,
+  },
+  nationalId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  cardImage: {
+    type: String,
+    required: true,
+  },
+  faceImage: {
+    type: String, 
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["pending", "verified", "rejected"],
+    default: "pending",
+  },
+}, { timestamps: true });
 
-export default mongoose.model("kyc" , kycSchema);
+export default mongoose.model("Kyc", kycSchema);
